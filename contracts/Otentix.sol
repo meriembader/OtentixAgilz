07f196152450a2f4007d0e8e3910b45eba9af532
meriembader <meriembader8@gmail.com>
2022-02-23 16:56:13 +0100

    mint collection solidity and front donneeeee

--- contracts/Otentix.sol
+++ contracts/Otentix.sol
@@ -8,39 +8,114 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract Otentix is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable, ERC721Burnable {
+contract Otentix is ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
-  using Strings for uint256;  
+      Counters.Counter private _tokenIdCounter;
+    using SafeMath for uint256;
+    Counters.Counter private _tokenIds;
+//the maxi nbre of nft that can be minted 
+    uint public constant MAX_SUPPLY = 100;
+    //the amount required to buy 1 nft 
+    uint public constant PRICE = 0.01 ether;
+    //the maxi nbre of mints per transaction
+    uint public constant MAX_PER_MINT = 5;
+//the ipfs url of the folder containing the json metadataaa
+    string public baseTokenURI;
+    using Strings for uint256;  
+    //list of existing uri
     mapping(string => uint8) existingURIs;
-  //  mapping(string => uint8) newItemsId;
- //   string  metadataURI;
-   uint256 public maxMintAmount = 20;
-       uint256 public cost = 100 ether;
-  mapping(address => bool) public whitelisted;
-uint256 public maxSupply = 1000;
- 
-     uint256 public _mintAmount= 1;
-    constructor() ERC721("Otentix", "NFT") {}
+   
+constructor(string memory baseURI) ERC721("Otentix", "nft") {
+     setBaseURI(baseURI);
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://";
+
+}
+ function _baseURI() internal 
+                    view 
+                    virtual 
+                    override 
+                    returns (string memory) {
+     return baseTokenURI;
+}
+function setBaseURI(string memory _baseTokenURI) public onlyOwner {
+     baseTokenURI = _baseTokenURI;
+}
+
+    /*
+function reserveNFTs() public onlyOwner {
+  //check the total number of NFTs minted
+     uint totalMinted = _tokenIds.current();
+     //test :  if there are enough NFTs to reserve
+     require(
+        totalMinted.add(10) < MAX_SUPPLY, "Not enough NFTs"
+     );
+     // if test ok: proceed to mint 10 nft 
+     for (uint i = 0; i < 10; i++) {
+          _mintSingleNFT();
+     }
+}*/
+function mintNFTs(uint _count) public payable {
+     uint totalMinted = _tokenIds.current();
+     //test 1 : have enough nfts for the caller to mint
+     require(
+       totalMinted.add(_count) <= MAX_SUPPLY, "Not enough NFTs!"
+     );
+     // test 2 : the caller has requested to mint more than 0 and less than the min the maxi nbre of nft allowed per transac
+     
+     require(
+       _count > 0 && _count <= MAX_PER_MINT, 
+       "Cannot mint specified number of NFTs."
+     );
+     // test 3 : the caaller has sent enough ether to mint the requested nbre of mint 
+     require(
+       msg.value >= PRICE.mul(_count), 
+       "Not enough ether to purchase NFTs."
+     );
+     
+     for (uint i = 0; i < _count; i++) {
+            _mintSingleNFT();
+     }
+}
+
+function _mintSingleNFT() private {
+  //get the current ID that hasn’t been minted yet
+      uint newTokenID = _tokenIds.current();
+      //assign the NFT ID to the account that called the function using _safemint
+      _safeMint(msg.sender, newTokenID);
+      // increment the token IDs counter by 1
+      _tokenIds.increment();
+}
+
+
+
+//to know which NFTs each user holds
+// return how many tokens a particular owner holds
+function tokensOfOwner(address _owner) 
+         external 
+         view 
+         returns (uint[] memory) {
+     uint tokenCount = balanceOf(_owner);
+     uint[] memory tokensId = new uint256[](tokenCount);
+     for (uint i = 0; i < tokenCount; i++) {
+          tokensId[i] = tokenOfOwnerByIndex(_owner, i);
+     }
+
+     return tokensId;
+}
+// to withdraw the contract’s entire balance
+function withdraw() public payable onlyOwner {
+     uint balance = address(this).balance;
+     require(balance > 0, "No ether left to withdraw");
+     (bool success, ) = (msg.sender).call{value: balance}("");
+     require(success, "Transfer failed.");
+}
+
+   function isContentOwned(string memory uri) public view returns (bool) {
+        return existingURIs[uri] == 1;
     }
 
-    function pause() public onlyOwner {
-        _pause();
-    }
-    function unpause() public onlyOwner {
-        _unpause();
-    }
-    function safeMint(address to, string memory uri) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-         existingURIs[uri] = 1;
-    }
 
   /*  function mintNft( address _to, uint _mintAmount) public payable{
         require(existingURIs[metadataURI] != 1, 'NFT already minted!');
@@ -60,7 +135,7 @@ uint256 public maxSupply = 1000;
        
     }
   */
-  function payToMint(address recipient, string memory metadataURI ) public payable returns (uint256) {
+ /* function payToMint(address recipient, string memory metadataURI ) public payable returns (uint256) {
         require(existingURIs[metadataURI] != 1, 'NFT already minted!');
         require (msg.value >= 0.1 ether, 'Need to pay up!');
          uint256 supply = totalSupply();
@@ -76,51 +151,20 @@ uint256 public maxSupply = 1000;
          }
           _setTokenURI(newItemsId, metadataURI);
         return newItemsId;
-    }
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        whenNotPaused
-        override(ERC721, ERC721Enumerable)
-    {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
+    }*/
+
 
     // The following functions are overrides required by Solidity.
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
-       function isContentOwned(string memory uri) public view returns (bool) {
-        return existingURIs[uri] == 1;
-    }
+  
        function count() public view returns (uint256) {
-        return _tokenIdCounter.current();
+        return _tokenIds.current();
     }
-     function whitelistUser(address _user) public onlyOwner {
-    whitelisted[_user] = true;
-  }
+   
  
-  function removeWhitelistUser(address _user) public onlyOwner {
-    whitelisted[_user] = false;
-  }
+  
 
 
 }
--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -1,19 +1,41 @@
+const { utils } = require("ethers");
 const hre = require("hardhat");
-
 async function main() {
+    const baseTokenURI = "ipfs://Qmf2kq9RaoQobYYb2Bzpv2zNGDVft4tuf6k7znSGV2k86f/";
 
-  const Otentix = await hre.ethers.getContractFactory("Otentix");
-  const nft = await Otentix.deploy();
+    // Get owner/deployer's wallet address
+    const [owner] = await hre.ethers.getSigners();
 
-  await nft.deployed();
+    // Get contract that we want to deploy
+    const contractFactory = await hre.ethers.getContractFactory("Otentix");
 
-  console.log("My NFT deployed to:", nft.address);
+    // Deploy contract with the correct constructor arguments
+    const contract = await contractFactory.deploy(baseTokenURI);
+
+    // Wait for this transaction to be mined
+    await contract.deployed();
+
+    // Get contract address
+    console.log("Contract deployed to:", contract.address);
+
+  /*  // Reserve NFTs
+    let txn = await contract.reserveNFTs();
+    await txn.wait();
+    console.log("10 NFTs have been reserved");
+*/
+  /*  // Mint 3 NFTs by sending 0.03 ether
+    txn = await contract.mintNFTs(3, { value: utils.parseEther('0.03') });
+    await txn.wait()
+*/
+    // Get all token IDs of the owner
+    let tokens = await contract.tokensOfOwner(owner.address)
+    console.log("Owner has tokens: ", tokens);
+
 }
 
-
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
