c9b71855133f0fd582d48447801e6546b0c73243
meriembader <meriembader8@gmail.com>
2022-05-16 18:11:28 +0100

    buy nft

--- contracts/Otentix.sol
+++ contracts/Otentix.sol
@@ -209,4 +209,20 @@ function payToMint(address recipient,string memory metadataURI ) public payable 
         _setTokenURI(newItemId, metadataURI);
         return newItemId;
     }
+
+function buyNft(uint tokenId) public payable {
+  
+    uint price = _idToNftItem[tokenId].price;
+    address owner = ERC721.ownerOf(tokenId);
+
+    require(msg.sender != owner, "You already own this NFT");
+    require(msg.value == price, "Please submit the asking price");
+
+    _idToNftItem[tokenId].isListed = false;
+    _listedItems.decrement();
+
+    _transfer(owner, msg.sender, tokenId);
+    payable(owner).transfer(msg.value);
+  }
+
 }
